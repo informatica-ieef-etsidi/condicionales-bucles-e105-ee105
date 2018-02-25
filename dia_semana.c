3a8f1b891a93e0242d0670463172553b1f5b91e4
Gonzalosanchezd6 <36692759+Gonzalosanchezd6@users.noreply.github.com>
2018-02-25 15:48:29 +0100

    Adivina el día de la semana
    Introduciendo un número del 1 al 7 te dice que día de la semana es (1=Lunes, 2=Martes...)

---
+++ dia_semana.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+	int numero;
+
+	printf("Por favor, introduzca un numero del 1 al 7\n");
+	scanf_s("%d", &numero);
+
+	switch (numero)
+	{
+	case 1: printf("Lunes\n");
+		break;
+	case 2: printf("Martes\n");
+		break;
+	case 3: printf("Miercoles\n");
+		break;
+	case 4: printf("Jueves\n");
+		break;
+	case 5: printf("Viernes\n");
+		break;
+	case 6: printf("Sabado\n");
+		break;
+	case 7: printf("Domingo\n");
+		break;
+	default:
+		printf("opcion incorrecta\n");
+	}
+
+	system("pause");
+}
