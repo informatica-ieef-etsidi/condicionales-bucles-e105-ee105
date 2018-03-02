61d73bb0c4cd553fde756fb8f3bbdee38450141d
JavierRedondoHernando <36537926+JavierRedondoHernando@users.noreply.github.com>
2018-03-02 16:56:06 +0100

    Corrección
    Tras introducir un intervalo erróneo 10 veces el programa se cerraba, ahora cumple el requisito de seguir pidiendo el intervalo hasta que sea correcto

--- Ejercicio1-bucles.c
+++ Ejercicio1-bucles.c
@@ -17,26 +17,27 @@ void main() {
 		printf("Introduzca el valor maximo del intervalo: ");
 		scanf_s("%d", &n2);
 
-		if (n1 < 0 || n1 > n2) {
-			printf("ERROR\n");
+		while (n1 < 0 || n1 > n2) {
+			printf("ERROR\n\n");
+			printf("Debe indicar entre que valores buscar los multiplos.\n\nIntroduzca el valor minimo del intervalo: ");
+			scanf_s("%d", &n1);
+			printf("Introduzca el valor maximo del intervalo: ");
+			scanf_s("%d", &n2);
 		}
-		else {
+		for (c = n1; c <= n2; c++) {
+			if (c % 7 == 0) {
+				b = 1;
+			}
+		}
+		if (b = 0) {
+			printf("No hay multiplos de 7 en el intervalo.\n");
+		}
+		if (b = 1) {
+			printf("Los multiplos de 7 en el intervalo [%d , %d] son: ", n1, n2);
 
 			for (c = n1; c <= n2; c++) {
 				if (c % 7 == 0) {
-					b = 1;
-				}
-			}
-			if (b = 0) {
-				printf("No hay multiplos de 7 en el intervalo.\n");
-			}
-			if (b = 1) {
-				printf("Los multiplos de 7 en el intervalo [%d , %d] son: ", n1, n2);
-
-				for (c = n1; c <= n2; c++) {
-					if (c % 7 == 0) {
-						printf("%d ", c);
-					}
+					printf("%d ", c);
 				}
 			}
 		}
