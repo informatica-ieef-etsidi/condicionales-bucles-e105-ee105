829f1825f692357324f4c00807fa27e2a1eea100
Marcos Velázquez Ruiz <36637024+MrCaos@users.noreply.github.com>
2018-03-02 17:53:55 +0100

    Sumatorio resuelto de una tercera forma.
    Sumatorio resuelto con for y while

---
+++ Sumatorio.c
@@ -0,0 +1,28 @@
+// Desarrollador: Marcos Velázquez Ruiz
+// Este programa calcula el sumatorio desde 1 hasta n 
+
+
+
+#include <stdio.h>
+void main() {
+
+	int n;
+	int suma = 0;
+	int i = 1;
+
+	printf("Introduce un numero\n");
+	scanf_s("%d", &n);
+
+
+	while (n < 0) {
+		printf("El numero debe ser mayor que 0.\n");
+		printf("Introduce un numero\n");
+		scanf_s("%d", &n);
+	}
+
+	for (i; i <= n; i++)
+			suma = suma + i;
+		printf("Sumatorio = %d\n", suma);
+	
+	system("pause");
+}
