e0ea6bca561f12c1b6e1ff0073e8a111847dd402
mariabosaba <36692966+mariabosaba@users.noreply.github.com>
2018-03-06 18:12:36 +0100

    Update ejercicio7_sentencias.c

--- ejercicio7_sentencias.c
+++ ejercicio7_sentencias.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /*Ejercicio 7
-Realice un programa en el que el usuario pueda seleccionar la operaci髇 matem醫ica a realizar
-con dos n鷐eros que introduce por teclado (suma, resta, multiplicaci髇, divisi髇, potencia). */
+Realice un programa en el que el usuario pueda seleccionar la operaci贸n matem谩tica a realizar
+con dos n煤meros que introduce por teclado (suma, resta, multiplicaci贸n, divisi贸n, potencia). */
 void main() {
-	float x, y,potencia=1,i;//Numeros que desee el usuario para realizar la operaci髇 matem醫ica, y contador para calcular potencia sin libreria math.h
+	float x, y,potencia=1,i;//Numeros que desee el usuario para realizar la operaci贸n matem谩tica, y contador para calcular potencia sin libreria math.h
 	char opcion;//S SUMA, R RESTA, M MULTIPLICAR, D DIVIDIR, P POTENCIA.
 	printf("Introduzca que operacion desea realizar: \n");
 	printf("1. S-para sumar \n");
@@ -16,28 +16,28 @@ void main() {
 
 	switch (opcion) {
 	case'S':
-		case's'://Para que valga opci髇 tanto en may鷖culas como en min鷖culas.
+		case's'://Para que valga opci贸n tanto en may煤sculas como en min煤sculas.
 		printf("Introduzca los numeros que quiere sumar: \n");
 		scanf_s("%f %f", &x, &y);
-		printf("La suma de %2.f y tanto %2.f es: %2.f\n", x, y, x + y);
+		printf("La suma de %.2f y tanto %.2f es: %.2f\n", x, y, x + y);
 		break;
 	case'R':
 		case'r':
 		printf("Introduzca los numeros que quiere restar: \n");
 		scanf_s("%f %f", &x, &y);
-		printf("La resta de %2.f y tanto %2.f es: %2.f\n", x, y, x - y);
+		printf("La resta de %.2f y tanto %.2f es: %.2f\n", x, y, x - y);
 		break;
 	case'M':
 		case'm':
 		printf("Introduzca los numeros que quiere multiplicar: \n");
 		scanf_s("%f %f", &x, &y);
-		printf("La multiplicacion de %2.f y tanto %2.f es: %2.f\n", x, y, x*y);
+		printf("La multiplicacion de %.2f y tanto %.2f es: %.2f\n", x, y, x*y);
 		break;
 	case'D':
 		case'd':
 		printf("Introduzca los numeros que quiere dividir: \n");
 		scanf_s("%f %f", &x, &y);
-		printf("La division de %2.f y tanto %2.f es: %2.f\n", x, y, x / y);
+		printf("La division de %.2f y tanto %.2f es: %.2f\n", x, y, x / y);
 		break;
 	case'P':
 		case'p':
@@ -46,10 +46,10 @@ void main() {
 		printf("Introduzca el exponente: \n");
 		scanf_s("%f", &y);
 
-		for(i=1;i<=y;i++){//bucle para realizar la exponencial.
+		for(i=1;i<=y;i++){//bucle para realizar la potencia.
 		   potencia*=x;		
 		   }
-		printf("%2.f elevado a %2.f es: %2.f\n", x, y, potencia);
+		printf("%.2f elevado a %.2f es: %.2f\n", x, y, potencia);
 		break;
 	default:
 		printf("ERROR.\n");
